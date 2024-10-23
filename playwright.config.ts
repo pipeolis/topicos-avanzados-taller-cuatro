be56d911803be97b2d4ef2528edf87681de6107a
Felipe Olis <felipeolis1985@gmail.com>
2024-10-22 21:46:51 -0500

    Refactor code

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ import {defineConfig, devices} from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './topicos-avanzados-taller-cuatro/tests',
+  testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
